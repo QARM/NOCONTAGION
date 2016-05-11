f0652e516f069d464496753663f9484e9ad8603d
QARM <laurent.wegerich@unil.ch>
2016-05-11 21:07:05 +0200

    Rename cols.m to VARDEMO/cols.m

---
+++ VARDEMO/cols.m
@@ -0,0 +1,16 @@
+function c=cols(X)
+
+
+
+% This function returns the number of columns in X.
+
+% Format:	c = cols(X)
+
+%
+
+% Lewis Chan, 11/8/98.
+
+
+
+[r,c]=size(X);
+
--- cols.m
+++
@@ -1,8 +0,0 @@
-function c=cols(X)
-
-% This function returns the number of columns in X.
-% Format:	c = cols(X)
-%
-% Lewis Chan, 11/8/98.
-
-[r,c]=size(X);
